bfd08efe5fa7492eae1e2bb53b35c027e072b872
despores <ernest.sakhabutdinov@mail.ru>
2024-07-12 20:06:39 +0300

    removed react components from html doc

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Header, Main } from ".";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Header />
+    <Main />
+  </React.StrictMode>
+);
