87c45de0db09d298d40d144bfd0151ea6c56929b
despores <ernest.sakhabutdinov@mail.ru>
2024-07-12 20:23:25 +0300

    revert changes

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Header, Main } from ".";
+import { Header, Main } from "./components";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
