a751b6649ac6462f58b76317f3967146e14ddd8d
despores <ernest.sakhabutdinov@mail.ru>
2024-07-12 22:04:35 +0300

    changed styles location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Footer, Header, Main } from "./components";
+import { Header, Main } from "./components";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
@@ -9,6 +9,5 @@ root.render(
   <React.StrictMode>
     <Header />
     <Main />
-    <Footer />
   </React.StrictMode>
 );
